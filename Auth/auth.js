9c469573a19651c80d9978516e3cf693e0bcb0f5
Cfortune11 <47123823+Cfortune11@users.noreply.github.com>
2020-02-03 21:03:11 -0500

    new

--- Auth/auth.js
+++ Auth/auth.js
@@ -1,1 +1,11 @@
-console.log("hello world.");
+const loginEmail = document.getElementById("login-email");
+const loginPassword = document.getElementById("login-password");
+const loginButton = document.getElementById("login-button");
+const loginMesssage = document.getElementById("login-message");
+
+
+
+loginButton.onclick = function() {
+	console.log(LoginEmail.value, loginPassword.value);
+	loginMessage.textContent = "you logged in.";
+};
