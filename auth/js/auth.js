92ca0e5fa79ba03dc53b99efa0a0466fb7e1841e
Cfortune11 <47123823+Cfortune11@users.noreply.github.com>
2020-02-10 20:49:30 -0500

    new

--- auth/js/auth.js
+++ auth/js/auth.js
@@ -2,11 +2,11 @@ const loginEmail = document.getElementById("login-email");
 const loginPassword = document.getElementById("login-password");
 const loginButton = document.getElementById("login-button");
 const loginMessage = document.getElementById("login-message");
-const username = document.getElementById("username");
+const loginUsername = document.getElementById("login-username");
 
 
 loginButton.onclick = function() {
-	fb.login(loginEmail.value,loginPassword.value)
+	fb.login(loginEmail.value,loginPassword.value);
 
 	//loginMessage.textContent = "You logged in.";
 
@@ -17,5 +17,5 @@ function onError(errorMessage){
 }
 
 function userLoggedIn(uid, displayName) {
-	userName.textContent = "Welcome" + displayName;
+	loginUsername.textContent = "Welcome" + displayName;
 }
